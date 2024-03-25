b832de25f9aeb17c69a7fd4cf6bff471745ccd67
Raymond Loman <raymond@carpago.nl>
2024-03-25 14:16:46 +0100

    Add class Assert with companion object demo

---
+++ src/main/kotlin/nl/acme/carapp/utils/App.kt
@@ -0,0 +1,28 @@
+package nl.acme.carapp.utils
+
+class Assert {
+    companion object {
+        fun assertEquals(expected: Any, actual: Any) {
+            assertTrue(expected == actual, "Expected: $expected, Actual: $actual")
+            assertOK("$actual (expected: $expected => OK)")
+        }
+        fun assertNotEquals(notExpected: Any, actual: Any) {
+            assertFalse(notExpected == actual, "Not expected: $notExpected, Actual: $actual")
+            assertOK("$actual (not expected: $notExpected => OK)")
+        }
+        fun assertFalse(booleanExpr: Boolean, message: String = "Expected $booleanExpr to be false") {
+            if (booleanExpr) {
+                throw AssertionError(message)
+            }
+        }
+        fun assertTrue(booleanExpr: Boolean, message: String = "Expected $booleanExpr to be true") {
+            if (!booleanExpr) {
+                throw AssertionError(message)
+            }
+            assertOK()
+        }
+        fun assertOK(message: String = "OK") {
+            println(message)
+        }
+    }
+}
