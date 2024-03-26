265dc9e75473e4fd7d3e30d2b3dddf35468bd092
Raymond Loman <raymond@carpago.nl>
2024-03-26 21:42:46 +0100

    Rocker for showing the use case when having multile beans. It rocks.

---
+++ src/main/kotlin/nl/acme/carapp/config/EmployeeBeans.kt
@@ -0,0 +1,25 @@
+package nl.acme.carapp.config
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+data class Employee(val name: String)
+
+
+@Configuration
+class EmployeeBeans {
+
+    @Bean
+    fun johnEmployee(): Employee {
+
+        return Employee("John")
+    }
+
+    @Primary
+    @Bean
+    fun tonyEmployee(): Employee {
+        return Employee("Tony")
+    }
+}
--- src/main/kotlin/nl/acme/carapp/service/CarService.kt
+++ src/main/kotlin/nl/acme/carapp/service/CarService.kt
@@ -1,19 +1,32 @@
 package nl.acme.carapp.service
 
 import jakarta.transaction.Transactional
+import nl.acme.carapp.config.Employee
 import nl.acme.carapp.model.Car
 import nl.acme.carapp.persistence.CarRepository
+import nl.acme.carapp.utils.Assert.Companion.assertEquals
 import nl.acme.carapp.utils.CarOwner
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import kotlin.jvm.optionals.getOrNull
 
+/*
+We fix the ambiguous bean issue by choosing between two options
+1. Using the qualifier with the name (which is the name of the method or the bean name)
+2. Using the @Primary annotation when defining the bean
+
+Adddional:
+to show that the beanName can be different than the method name, see bean with name johnEmployee in EmployeeBeans
+ */
 
 @Service
-class CarService(val repository: CarRepository, bmw: Car, jan: CarOwner) {
+class CarService(val repository: CarRepository, val bmw: Car, val jan: CarOwner, /*@Qualifier("JohnEmployee")*/ val employee: Employee,  @Qualifier("johnEmployee") val johnEmployee: Employee) {
 
     init {
         this.repository.save(bmw)
-        println("Car breated ${bmw} for owner: ${jan}")
+        println("Car breated ${bmw} for owner: ${this.jan}")
+        assertEquals("John", this.johnEmployee.name)
     }
 
     fun getAllCars(): List<Car> {
