5cd9257fa76f8d9456156b0c227aec0ef406d455
Raymond Loman <raymond@carpago.nl>
2024-03-26 21:47:28 +0100

    Personal review mods

--- src/main/kotlin/nl/acme/carapp/config/EmployeeBeans.kt
+++ src/main/kotlin/nl/acme/carapp/config/EmployeeBeans.kt
@@ -1,6 +1,5 @@
 package nl.acme.carapp.config
 
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
