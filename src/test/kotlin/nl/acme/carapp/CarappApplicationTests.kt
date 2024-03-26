09a4bdb9a7b85938d3e957b87d32ce7b57b2d566
Raymond Loman <raymond@carpago.nl>
2024-03-26 22:04:52 +0100

    Add demo for the reading order of application-*.properties files

--- src/test/kotlin/nl/acme/carapp/CarappApplicationTests.kt
+++ src/test/kotlin/nl/acme/carapp/CarappApplicationTests.kt
@@ -1,13 +1,17 @@
 package nl.acme.carapp
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CarappApplicationTests {
 
+	@Value("\${spring.application.name}")
+	lateinit var name: String;
+
 	@Test
 	fun contextLoads() {
+		println(this.name)
 	}
-
 }
