a126e3799a36c233acdf79512982ef06fbe96af5
Raymond Loman <raymond@carpago.nl>
2024-03-27 09:59:47 +0100

    Add when

--- src/main/kotlin/nl/acme/kotlindemo/starprojections/App.kt
+++ src/main/kotlin/nl/acme/kotlindemo/starprojections/App.kt
@@ -3,14 +3,21 @@ package nl.acme.kotlindemo.starprojections
 import nl.acme.carapp.model.Car
 
 fun main() {
-    println(listOf(1,3.5, "String", Car()))
+    val listElement = printList(listOf(1, 3.5, "String", Car()))
 
+    when (listElement) {
+        is Double -> {
+            println("Het is een double: ${listElement}")
+        }
+
+        is String -> {
+            println("Het is een String: ${listElement}")
+        }
+    }
 }
 
-
-
-fun printList(list: List<*>) {
+fun printList(list: List<*>): Any? {
     println(list)
 //    list.addFirst("") // faalt want het is geen consumer!
-//    return list.get(0) // faalt want het is geen producer (dus * is geen consumer en geen producer maar kan inderdaad alles ontvangen)
+    return list.get(0) // faalt (althans, er komt een Any uit) want het is geen producer (dus * is geen consumer en geen producer maar kan inderdaad alles ontvangen)
 }
